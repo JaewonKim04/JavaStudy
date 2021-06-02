0ccae098276c895f45f77f8dd05f58712800be9f
jaewonkim1468 <turitus@naver.com>
2021-06-02 11:14:56 +0900

    [day_0602] hash set

---
+++ src/day_0602_collection_framework/example_hashset/HashSetExample.java
@@ -0,0 +1,30 @@
+package day_0602_collection_framework.example_hashset;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSetExample {
+	public static void main(String[] args) {
+		Set<String> set = new HashSet<String>();
+		
+		set.add("Java");
+		set.add("Spring");
+		set.add("NetWork");
+		set.add("Java");
+		set.add("OS");
+		
+		int size = set.size();
+		System.out.println("총 객체수: " + size);
+		
+		Iterator<String> iterator = set.iterator();
+		while(iterator.hasNext()) {
+			String element = iterator.next();
+			System.out.println("\t"+element);
+		}
+		set.remove("Spring");
+		set.remove("OS");
+		
+		System.out.println("총 객체수: " + set.size());
+	}
+}
