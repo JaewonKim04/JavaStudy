4445003c5b628deb4c1967893af399c731df82ba
JaeWonKim <turitus@naver.com>
2021-06-22 08:50:56 +0900

    [anonymous]익명 구현, 자식 객체

--- src/day_0428_anonymous/first/MainTest.java
+++ src/day_0428_anonymous/first/MainTest.java
@@ -1,20 +1,21 @@
 package day_0428_anonymous.first;
 
 public class MainTest {
-	public static void main(String[] args) {
-		Anonymous anony = new Anonymous();
-		anony.field.wake();
-		anony.method1();
-		anony.method2(new Person() {
-			void study() {
-				System.out.println("공부합니다.");
-			}
-			@Override
-			void wake() {
-				System.out.println("8시에 일어납니다.");
-				study();
-			}
-		});
-	}
+    public static void main(String[] args) {
+        Anonymous anony = new Anonymous();
+        anony.field.wake();
+        anony.method1();
+        anony.method2(new Person() {
+            void study() {
+                System.out.println("공부합니다.");
+            }
+
+            @Override
+            void wake() {
+                System.out.println("8시에 일어납니다.");
+                study();
+            }
+        });
+    }
 
 }
